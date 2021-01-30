1b36ff481ff2c91826b27ac4350c9d3e7c965353
Anirudhya Duyti <adchinmoy@gmail.com>
2021-01-30 18:19:09 +0600

    final v.1.1 commented

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// global variables
+// global variable
 var notReturning = true;    //  to check if customer choose a return date
 
-// update count
+// update count on each +/-
 function updateCount(idPart, isIncrease) {
     const caseCount = getCount(idPart + "Count");
-    document.getElementById(idPart + "Count").value = caseCount + (isIncrease ? 1 : (caseCount <= 0) ? 0 : -1);
+    document.getElementById(idPart + "Count").value = caseCount + (isIncrease ? 1 : (caseCount <= 0) ? 0 : -1);     // set the count according to +/- and previous value
 
-    updateCalculation();
+    updateCalculation();            // update the calculations each time event occurred 
 }
 
 // get value from input
 function getCount(id) {
     const count = document.getElementById(id).value;
-    if (count == "") {
+    if (count == "") {          // if NaN set to 0
         return 0;
     }
     else {
@@ -25,27 +25,30 @@ function updateCalculation() {
     const economyClassUnitPrice = 100;
     const firstClassCount = getCount("firstClassCount");
     const economyClassCount = getCount("economyClassCount");
-    const subtotalPrice = ((firstClassCount * firstClassUnitPrice) + (economyClassCount * economyClassUnitPrice)) * (notReturning ? 1 : 2);
+    const subtotalPrice = ((firstClassCount * firstClassUnitPrice) + (economyClassCount * economyClassUnitPrice)) * (notReturning ? 1 : 2);     //check if trip is 2-way to double the fare
+
+    // updating new price
     document.getElementById("subtotalPrice").innerText = "$" + subtotalPrice;
     document.getElementById("vat").innerText = "$" + Math.round(subtotalPrice * .10);
     document.getElementById("totalPrice").innerText = "$" + (subtotalPrice + Math.round(subtotalPrice * .10));
 }
 
 
-// date function
+// to calculate current date
 var today = new Date();
 var date = today.getDate();
 var month = today.getMonth() + 1;
-
 var day = today.getFullYear() + (month >= 10 ? "-" : "-0") + month + (date >= 10 ? "-" : "-0") + date;
-document.getElementById("departureDate").min = day;
+
+document.getElementById("departureDate").min = day;         // set minimum departure date to today 
 
 function setReturnDateMin() {
-    document.getElementById("returnDate").min = document.getElementById("departureDate").value;
+    document.getElementById("returnDate").min = document.getElementById("departureDate").value;     // set minimum return date to departure date
 }
 
+// set return date
 function updateForReturn() {
-    if (document.getElementById("departureDate").value == "") {
+    if (document.getElementById("departureDate").value == "") {         // cannot set return date without setting departure date
         document.getElementById("returnDate").value = "";
         alert("Please, First Choose A Departure Date ");
     }
@@ -55,20 +58,22 @@ function updateForReturn() {
     }
 }
 
-// Booking Button
+// Booking Button listener
 document.getElementById("bookingBtn").addEventListener('click', function () {
-    if (checkAndSetValues() && this.innerText == "Book Now") {
-        document.getElementById("bookingForm").style.display = "none";
-        document.getElementById("invoice").style.display = "block";
-        setInvoice();
+    if (checkInputs() && this.innerText == "Book Now") {                  // checking booking complete
+        document.getElementById("bookingForm").style.display = "none";          // hiding the form
+        document.getElementById("invoice").style.display = "block";             // displaying invoice which was hidden
+        setInvoice();                                                           // setting invoice to show it
         this.innerText = "Book Another Cruise Trip";
     }
-    else if (this.innerText == "Book Another Cruise Trip") {
-        location.reload();
+    else if (this.innerText == "Book Another Cruise Trip") {                    // only while invoice is showing
+        location.reload();                                                      // clicking the button wil reload the page
     }
 });
 
-function checkAndSetValues() {
+
+// check for all necessary inputs. if empty show alert and booking will not proceed
+function checkInputs() {
     if (document.getElementById("from").value == "") {
         alert("Please, Enter Your Departure Location");
     }
@@ -86,6 +91,9 @@ function checkAndSetValues() {
     }
     return false;
 }
+
+
+// setting the invoice
 function setInvoice() {
     let bookingType = document.getElementById("booking-Type");
     let cruiseRoute = document.getElementById("cruise-route");
